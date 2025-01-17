f3a246b36736f2c3dcb75637aa186564f68d8988
Hermes <hermesbarbosa9@gmail.com>
2025-01-16 21:52:55 -0300

    Updates

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,6 +1,6 @@
 import functools
 from flask import (
-    Blueprint, request, redirect, render_template, g, url_for, flash, session
+    Blueprint, render_template, redirect, url_for, g, request, flash, session
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 from .db import get_db
@@ -12,56 +12,56 @@ def register():
     if request.method=='POST':
         username = request.form['username']
         password = request.form['password']
-        db = get_db()
         error = None
+        db = get_db()
 
-        if not username:
-            error = 'O nome de usuário é obrigatório!'
-        elif not password:
+        if username is None:
+            error = 'O usuário é obrigatório!'
+        elif password is None:
             error = 'A senha é obrigatória!'
 
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    'INSERT INTO user (username, password) VALUES (?, ?)',
                     (username, generate_password_hash(password))
                 )
             except db.IntegrityError:
-                error = f'O usuário {username} já está cadastrado!'
+                error = 'Usuário já cadastrado!'
             else:
                 return redirect(url_for('auth.login'))
-
+        
         flash(error)
 
     return render_template('auth/register.html')
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method=='POST':
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        db = get_db()
         error = None
+        db = get_db()
 
         user = db.execute(
             'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+        ).fetchone
 
         if user is None:
             error = 'Usuário inválido!'
-        elif not check_password_hash(user['password'], password):
+        elif not check_password_hash(user['password', password]):
             error = 'Senha inválida!'
         
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            redirect(url_for('index'))
 
-        flash(error)
-    
+        flash(error) 
+
     return render_template('auth/login.html')
 
-@bp.before_app_request
+@bp.before_request
 def load_logged_in_user():
     user_id = session.get('user_id')
 
@@ -69,19 +69,19 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id)
-        ).fetchone()
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone
 
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    redirect(url_for('index'))
 
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return redirect(url_for('auth.login'))
+            return redirect(url_for('index'))
         return view(**kwargs)
     
     return wrapped_view
